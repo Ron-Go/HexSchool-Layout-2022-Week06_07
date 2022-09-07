4872738a0f4f0dc83b45d1d0094f6ad43865b5c6
Ron <hpleain@gmail.com>
2022-09-07 09:56:21 +0800

    Update 2022-09-07T01:56:17.714Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,27 +1,26 @@
-let hamburger = document.querySelector('.header__hamburger');
-hamburger.addEventListener('click', (e) => {
+"use strict";
+
+var hamburger = document.querySelector('.header__hamburger');
+hamburger.addEventListener('click', function (e) {
   e.preventDefault();
   document.querySelector('.header__nav').classList.toggle('header__nav--active');
   document.querySelector('.header__hamburger').classList.toggle('header__hamburger--active');
-});
+}); // Swiper
 
-
-// Swiper
-const swiper = new Swiper('.swiper', {
+var swiper = new Swiper('.swiper', {
   // Optional parameters
   effect: 'fade',
   fadeEffect: {
     crossFade: true
   },
-  
   // If we need pagination
   pagination: {
-    el: '.swiper-pagination',
+    el: '.swiper-pagination'
   },
-
   // Navigation arrows
   navigation: {
     nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
+    prevEl: '.swiper-button-prev'
+  }
 });
+//# sourceMappingURL=all.js.map
